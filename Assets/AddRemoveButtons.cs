c25e734e2afdc2303a3a4041b32f4619bfdff2b1
Janikowski, Josh <janikows@fvtc.edu>
2019-11-10 14:15:39 -0600

    works and startign the button work

--- Assets/AddRemoveButtons.cs
+++ Assets/AddRemoveButtons.cs
@@ -1,11 +1,16 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AddRemoveButtons : MonoBehaviour
 {
     public GameObject addPlayer;
     public GameObject removePlayer;
+    public GameObject textarea;
+   //ublic Text playerName;
+  //public Text inputplayerText;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,11 +23,15 @@ public class AddRemoveButtons : MonoBehaviour
         {
             this.removePlayer.SetActive(true);
             this.addPlayer.SetActive(false);
+           //extarea.SetActive(false);
+          //playerName.text = inputplayerText.text;
         }
         else
         {
             this.removePlayer.SetActive(false);
             this.addPlayer.SetActive(true);
+           //extarea.SetActive(true);
+           //layerName.text = "";
         }
     }
     // Update is called once per frame
---
+++ Assets/ButtonPositions.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonPositions : MonoBehaviour
+{
+    public Vector3[] positions;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void SetButtonPosition(int pos)
+    {
+        //foreach(Vector3 position in positions)
+        //{
+        //    if (this.transform.position == position)
+        //    {
+        //        Debug.Log("hit it");
+        //    }
+        //}
+
+    }
+}
